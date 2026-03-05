222ecc26bf8efbdeb6855ab48bd3045977c03cca
Keys <70819367+keys-i@users.noreply.github.com>
2026-03-05 21:18:37 +1000

    moved exploration notebook to main and removed data folder
    minor refactor in get_dataset to make it more shorter
    
    Signed-off-by: Keys <70819367+keys-i@users.noreply.github.com>

--- scripts/get_dataset.py
+++ scripts/get_dataset.py
@@ -10,9 +10,9 @@ from huggingface_hub import hf_hub_download
 
 REPO_ID = "keys-i/netFlow-v3"
 FILENAME = "v3/pqt/NF-CICIDS2018-v3.parquet"
-DESTINATION = Path("data/NF-CICIDS2018-v3.parquet")
+DST = Path("data/NF-CICIDS2018-v3.parquet")
 
-DESTINATION.parent.mkdir(parents=True, exist_ok=True)
+DST.parent.mkdir(parents=True, exist_ok=True)
 
 cached_path = hf_hub_download(
     repo_id=REPO_ID,
@@ -20,5 +20,5 @@ cached_path = hf_hub_download(
     repo_type="dataset",
 )
 
-shutil.copy2(cached_path, DESTINATION)
-print(f"downloaded: {DESTINATION.resolve()}")
+shutil.copy2(cached_path, DST)
+print(f"downloaded: {DST.resolve()}")
